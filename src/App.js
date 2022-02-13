7ae248a2b44441df3a331ea8d81b76ad89d978fe
Sakshi Walode <sakshiwalode28@gmail.com>
2022-02-13 05:40:41 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import img1 from "./light-bulb.png";
+import img from "./quote.png";
 
 function App() {
+  const [newQuote, setNewQuote] = useState("");
+  const [newAuthor, setNewAuthor] = useState("");
+
+  const fetchAdvice = (e) => {
+    e.preventDefault();
+    axios
+      .get("https://api.quotable.io/random")
+      .then((response) => {
+        console.log(response);
+        const { content, author } = response.data;
+        console.log(content);
+        setNewQuote(content);
+        setNewAuthor(author);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app_body">
+        {newQuote === "" ? (
+          <div className="no_quote">
+            <div className="text">
+            <img src={img1} alt="quote" className="img1" />
+           Give a start to your day with a great quote!
+            </div>
+            <button onClick={fetchAdvice} className="img1_btn">
+             Get A Quote
+            </button>
+          </div>
+        ) : (
+          <>
+            <div className="quote">{newQuote}</div>
+            <div className="quote_author">
+              <hr />
+              <div className="author">~{newAuthor}</div>
+            </div>
+            <button onClick={fetchAdvice}>
+              <div className="btn-content">
+                New Quote
+                <img src={img} alt="quote" className="img" />
+              </div>
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
